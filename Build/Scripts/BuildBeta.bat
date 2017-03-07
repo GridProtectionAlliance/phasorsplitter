7c7102807561626d090b3764f02899f09b93dd0a
gsfbuildbot <support@gridprotectionalliance.org>
2017-03-07 16:14:34 -0500

    Added Build/Scripts folder to the repository.

---
+++ Build/Scripts/BuildBeta.bat
@@ -0,0 +1,31 @@
+::*******************************************************************************************************
+::  BuildBeta.bat - Gbtc
+::
+::  Copyright © 2017, Grid Protection Alliance.  All Rights Reserved.
+::
+::  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+::  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+::  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may
+::  not use this file except in compliance with the License. You may obtain a copy of the License at:
+::
+::      http://opensource.org/licenses/MIT
+::
+::  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+::  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+::  License for the specific language governing permissions and limitations.
+::
+::  Code Modification History:
+::  -----------------------------------------------------------------------------------------------------
+::  03/07/2017 - Stephen C. Wills
+::       Generated original version of source code.
+::
+::*******************************************************************************************************
+
+@ECHO OFF
+
+SetLocal
+
+IF NOT "%1" == "" SET logflag=/l:FileLogger,Microsoft.Build.Engine;logfile=%1
+
+ECHO BuildBeta: C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe StreamSplitter.buildproj /p:ForceBuild=true %logflag%
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" StreamSplitter.buildproj /p:ForceBuild=true %logFlag%
---
+++ Build/Scripts/UpdateDependencies.bat
@@ -0,0 +1,87 @@
+::*******************************************************************************************************
+::  UpdateDependencies.bat - Gbtc
+::
+::  Copyright © 2017, Grid Protection Alliance.  All Rights Reserved.
+::
+::  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+::  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+::  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may
+::  not use this file except in compliance with the License. You may obtain a copy of the License at:
+::
+::      http://opensource.org/licenses/MIT
+::
+::  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+::  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+::  License for the specific language governing permissions and limitations.
+::
+::  Code Modification History:
+::  -----------------------------------------------------------------------------------------------------
+::  03/07/2017 - Stephen C. Wills
+::       Generated original version of source code.
+::
+::*******************************************************************************************************
+
+@ECHO OFF
+
+SETLOCAL
+
+SET pwd=%CD%
+IF "%git%" == "" SET git=%PROGRAMFILES(X86)%\Git\cmd\git.exe
+IF "%replace%" == "" SET replace=\\GPAWEB\NightlyBuilds\Tools\ReplaceInFiles\ReplaceInFiles.exe
+
+SET defaulttarget=%LOCALAPPDATA%\Temp\StreamSplitter
+IF "%remote%" == "" SET remote=git@github.com:GridProtectionAlliance/phasorsplitter.git
+IF "%gsf%" == "" SET gsf=\\GPAWEB\NightlyBuilds\GridSolutionsFramework\Beta
+IF "%target%" == "" SET target=%defaulttarget%
+
+SET gsflibraries=%gsf%\Libraries\*.*
+SET gsfdependencies=%target%\Source\Dependencies\GSF
+SET sourcemasterbuild=%gsf%\Build Scripts\MasterBuild.buildproj
+SET targetmasterbuild=%target%\Build\Scripts
+
+ECHO.
+ECHO Entering working directory...
+IF EXIST "%target%" IF NOT EXIST "%target%"\.git RMDIR /S /Q "%target%"
+IF NOT EXIST "%target%" MKDIR "%target%"
+CD /D %target%
+
+IF NOT EXIST .git GOTO CloneRepository
+IF NOT "%target%" == "%defaulttarget%" GOTO UpdateDependencies
+GOTO UpdateRepository
+
+:CloneRepository
+ECHO.
+ECHO Getting latest version...
+"%git%" clone "%remote%" .
+GOTO UpdateDependencies
+
+:UpdateRepository
+ECHO.
+ECHO Updating to latest version...
+"%git%" fetch
+"%git%" reset --hard origin/master
+"%git%" clean -f -d -x
+GOTO UpdateDependencies
+
+:UpdateDependencies
+ECHO.
+ECHO Updating dependencies...
+XCOPY "%gsflibraries%" "%gsfdependencies%\" /E /U /Y
+XCOPY "%sourcemasterbuild%" "%targetmasterbuild%\" /Y
+
+:CommitChanges
+ECHO.
+ECHO Committing updates to local repository...
+"%git%" add .
+"%git%" commit -m "Updated GSF dependencies."
+IF NOT "%donotpush%" == "" GOTO Finish
+
+:PushChanges
+ECHO.
+ECHO Pushing changes to remote repository...
+"%git%" push
+CD /D %pwd%
+
+:Finish
+ECHO.
+ECHO Update complete
